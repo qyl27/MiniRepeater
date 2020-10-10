d5a0344f55623c405bd7cfe953997534f8cbacd3
秋雨落 <i@rain.cx>
2020-10-10 13:17:05 +0800

    完成，等待测试。
    Signed-off-by: 秋雨落 <i@rain.cx>

--- cx.rain.qqmini.repeater/Repeater.cs
+++ cx.rain.qqmini.repeater/Repeater.cs
@@ -2,14 +2,15 @@
 using QQMini.PluginSDK.Core.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cx.rain.qqmini.repeater
 {
     public class Repeater : PluginBase
     {
+        private static readonly Dictionary<Group, Tuple<QQ, Message>> LastMessages = new Dictionary<Group, Tuple<QQ, Message>>();
+        private static readonly Dictionary<Group, HashSet<QQ>> MessageSenders = new Dictionary<Group, HashSet<QQ>>();
+        private static readonly Dictionary<Group, Message> RepeatedMessages = new Dictionary<Group, Message>();
+
         public override PluginInfo PluginInfo => new PluginInfo()
         {
             Author = "秋雨落",
@@ -18,5 +19,41 @@ namespace cx.rain.qqmini.repeater
             PackageId = "cx.rain.qqmini.repeater",
             Version = new Version(1, 0, 0, 0)
         };
+
+        public override void OnInitialize()
+        {
+            // Todo: Configuration files.
+        }
+
+        public override void OnUninitialize()
+        {
+            // Todo: Configuration files.
+        }
+
+        public override QMEventHandlerTypes OnReceiveGroupMessage(QMGroupMessageEventArgs e)
+        {
+            LastMessages[e.FromGroup] = new Tuple<QQ, Message>(e.FromQQ, e.Message);
+
+            if (MessageSenders[e.FromGroup] == null)
+            {
+                MessageSenders[e.FromGroup] = new HashSet<QQ>();
+            }
+            MessageSenders[e.FromGroup].Add(e.FromQQ);
+
+            if (RepeatedMessages[e.FromGroup] != null && e.Message == RepeatedMessages[e.FromGroup])
+            {
+                MessageSenders[e.FromGroup].Clear();
+                return QMEventHandlerTypes.Continue;
+            }
+
+            if (MessageSenders[e.FromGroup].Count >= 3)
+            {
+                QMApi.SendGroupMessage(e.RobotQQ, e.FromGroup, e.Message);
+                RepeatedMessages[e.FromGroup] = e.Message;
+                MessageSenders[e.FromGroup].Clear();
+            }
+
+            return QMEventHandlerTypes.Continue;
+        }
     }
 }
