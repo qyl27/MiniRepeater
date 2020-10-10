009dc2d2addc835b0c96e4ca9b830b7411d626cd
秋雨落 <i@rain.cx>
2020-10-10 12:38:33 +0800

    Init

---
+++ cx.rain.qqmini.repeater/Repeater.cs
@@ -0,0 +1,22 @@
+﻿using QQMini.PluginSDK.Core;
+using QQMini.PluginSDK.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cx.rain.qqmini.repeater
+{
+    public class Repeater : PluginBase
+    {
+        public override PluginInfo PluginInfo => new PluginInfo()
+        {
+            Author = "秋雨落",
+            Description = "一个简单的群聊复读机插件。",
+            Name = "Mini复读机",
+            PackageId = "cx.rain.qqmini.repeater",
+            Version = new Version(1, 0, 0, 0)
+        };
+    }
+}
